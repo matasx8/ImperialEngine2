1443717069dda11f11df45958eb5610e23134527
matasx8 <m.tunkevicius@gmail.com>
2024-10-24 09:02:08 +0300

    Implemented physical device selection. So far selecting first one since there's no need for advanced selection and mobile will likely have only one.

--- Source/Engine.cpp
+++ Source/Engine.cpp
@@ -26,6 +26,13 @@ namespace imp
             return result;
         g_Log("Vulkan Instance was successfully created.");
 
+        volkLoadInstance(m_Instance);
+
+        result = SelectPhysicalDevice(params);
+        if (result != VK_SUCCESS)
+            return result;
+        g_Log("Vulkan Physical Device was successfully created.");
+
         return result;
     }
 
@@ -78,8 +85,44 @@ namespace imp
         return result;
     }
 
-    VkResult Engine::DestroyInstance()
+    void Engine::DestroyInstance()
     {
         vkDestroyInstance(m_Instance, nullptr);
+    }
+
+    VkResult Engine::SelectPhysicalDevice(const EngineCreateParams& params)
+    {
+        uint32_t deviceCount = 0;
+        VkResult result = vkEnumeratePhysicalDevices(m_Instance, &deviceCount, nullptr);
+        if (result != VK_SUCCESS)
+            return result;
+
+        if (deviceCount == 0)
+        {
+            g_Log("Error: Could not find any Vulkan Physical Devices.\n");
+            return VK_ERROR_FEATURE_NOT_PRESENT;
+        }
+
+        std::vector<VkPhysicalDevice> devices { deviceCount };
+        result = vkEnumeratePhysicalDevices(m_Instance, &deviceCount, devices.data());
+        if (result != VK_SUCCESS)
+            return result;
+
+        VkPhysicalDeviceProperties props;
+        for (const auto& device : devices)
+        {
+            vkGetPhysicalDeviceProperties(device, &props);
+
+            // TODO: use pNext for custom selection when needed, so far selecting first available since on mobile there will likely be only one
+            if (true)
+            {
+                m_PhysicalDevice = device;
+
+                g_Log("Selected Physical Device: \"%s\"\n", props.deviceName);
+                return VK_SUCCESS;
+            }
+        }
+
+        return VK_ERROR_DEVICE_LOST;
     }
 }
--- Source/Engine.h
+++ Source/Engine.h
@@ -24,6 +24,9 @@ namespace imp
         VkResult CreateInstance(const EngineCreateParams& params);
         void DestroyInstance();
 
+        VkResult SelectPhysicalDevice(const EngineCreateParams& params);
+
         VkInstance m_Instance = VK_NULL_HANDLE;
+        VkPhysicalDevice m_PhysicalDevice = VK_NULL_HANDLE;
     };   
 }
